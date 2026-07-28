765be8605d74b7e983c472437f3332abd9decd5e
벨라 智慧 (zhì huì) <ameliaparka23@gmail.com>
2026-07-28 16:52:30 -0400

    Update docxread.py

--- QGen/reads/docxread.py
+++ QGen/reads/docxread.py
@@ -1,1 +1,13 @@
+from docx import Document
 
+def read_docx(file):
+
+    document = Document(file)
+
+    text = []
+
+    for paragraph in document.paragraphs:
+
+        text.append(paragraph.text)
+
+    return "\n".join(text)
