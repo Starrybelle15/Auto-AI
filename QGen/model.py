dda3834c64aa43aabded43dd85b79f8c1d149386
벨라 智慧 (zhì huì) <ameliaparka23@gmail.com>
2026-07-28 16:12:13 -0400

    Update model.py

--- QGen/model.py
+++ QGen/model.py
@@ -1,1 +1,14 @@
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
 
+MODEL_NAME = "google/flan-t5-base"
+
+print("Loading tokenizer...")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+print("Loading model...")
+
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+
+print("Model loaded successfully!")
