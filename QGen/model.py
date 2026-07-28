f0c1d7dd0d31e9e23bc9ba2b16dc8ac746dc2c90
벨라 智慧 (zhì huì) <ameliaparka23@gmail.com>
2026-07-28 16:49:01 -0400

    Update model.py

--- QGen/model.py
+++ QGen/model.py
@@ -1,7 +1,10 @@
+import torch
 from transformers import AutoTokenizer
 from transformers import AutoModelForSeq2SeqLM
 
 MODEL_NAME = "google/flan-t5-base"
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Loading tokenizer...")
 
@@ -11,4 +14,8 @@ print("Loading model...")
 
 model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
 
+model.to(device)
+
 print("Model loaded successfully!")
+
+print("Model Ready!")
