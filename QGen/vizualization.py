b7f36d335200d059c373aae7aa54403b5324dbbf
벨라 智慧 (zhì huì) <ameliaparka23@gmail.com>
2026-08-01 13:37:38 -0400

    Update vizualization.py

--- QGen/vizualization.py
+++ QGen/vizualization.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import plotly.express as px
 
+def sentiment_chart(sentiments):
+
+    df = pd.DataFrame(sentiments)
+
+    fig = px.pie(
+        df,
+        names="label",
+        title="Sentiment Distribution"
+    )
+
+    return fig
